1702e3785a5ca2c580cbbc9914a2e9f18ad52a2f
huydo862003 <huydo862003@users.noreply.github.com>
2026-06-28 12:47:37 +0000

    deploy: 3a7983aa754640ee9f693f9277f99de67bbd30b0

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9369f68a.js',
+                path_to_root + 'searchindex-75b536ab.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
